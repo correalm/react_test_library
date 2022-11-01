30fb4567711c79ff9c5b66e4150e249c02fcdd5a
Miguel <luismigueldasilva.99@gmail.com>
2022-11-01 17:54:04 -0300

    feat: section 4 ok

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,7 +1,8 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { queryByRole, render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import SummaryForm from '../SummaryForm'
 
-test('checkbox is disable by default and enables the button when is checked', async () => {
+test('inital conditions', async () => {
   render(<SummaryForm />)
 
   const checkbox = await screen.findByRole('checkbox', { name: 'I agree to Terms and Conditions' })
@@ -9,12 +10,38 @@ test('checkbox is disable by default and enables the button when is checked', as
   
   expect(checkbox).not.toBeChecked()
   expect(btn).toBeDisabled()
+})
 
-  fireEvent.click(checkbox)
+test('should enable the button when is checked and disable him again', async () => {
+  const user = userEvent.setup()
+  render(<SummaryForm />)
+
+  const checkbox = await screen.findByRole('checkbox', { name: 'I agree to Terms and Conditions' })
+  const btn = await screen.findByRole('button', { name: 'Confirm order' })
+
+  await user.click(checkbox)
   expect(checkbox).toBeChecked()
   expect(btn).toBeEnabled()
 
-  fireEvent.click(checkbox)
+  await user.click(checkbox)
   expect(checkbox).not.toBeChecked()
   expect(btn).toBeDisabled()
+})
+
+test('popover responds to hover', async () => {
+  const user = userEvent.setup()
+  render(<SummaryForm />)
+
+  const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i)
+  expect(nullPopover).not.toBeInTheDocument()
+
+  // simulate the mousehover and appears the popover
+  const termsAndConditions = screen.getByText(/terms and conditions/i)
+  
+  await user.hover(termsAndConditions)
+  const popover = screen.getByText(/no ice cream will actually be delivered/i)
+  expect(popover).toBeInTheDocument()
+
+  await user.unhover(termsAndConditions)
+  expect(popover).not.toBeInTheDocument()
 })
