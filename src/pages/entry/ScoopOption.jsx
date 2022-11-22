568e4aea5e1f18a9bee28dba0c22205a6ad73bad
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2022-11-22 09:58:58 -0300

    feat: create ScoopOption and OrderSummary components

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,6 +1,11 @@
-import Col from "react-bootstrap/Col";
+import { Form, Col, Row } from 'react-bootstrap'
+import { useOrderDetails } from '../../contexts/OrderDetails'
 
 export default function ScoopOptions({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails()
+
+  const handleChange = (e) => updateItemCount(name, parseInt(e.target.value), 'scoops')
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -8,6 +13,18 @@ export default function ScoopOptions({ name, imagePath }) {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} scoop`}
       />
+      <Form.Group controlId={`${name}-count`} as={Row} style={{ marginTop: '10px' }}>
+        <Form.Label column xs='6' style={{ textAlign: 'right' }}>
+          { name }
+          <Col xs='5' style={{textAlign: 'right'}}>
+            <Form.Control
+              type='number'
+              defaultValue={ 0 }
+              onChange={ handleChange }
+            />
+          </Col>
+        </Form.Label>
+      </Form.Group>
     </Col>
   );
 }
